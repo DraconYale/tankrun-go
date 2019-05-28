cb5390fd49443836d8d581a6058cc5cb8e910050
gianluca <gianlucalup@gmail.com>
2019-05-28 19:26:52 +0200

    Very buggy jump

--- tankrun-go.go
+++ tankrun-go.go
@@ -5,24 +5,41 @@ import (
     _ "image/png"
     "log"
     "fmt"
+    "time"
     "github.com/hajimehoshi/ebiten"
     "github.com/hajimehoshi/ebiten/ebitenutil"
+    "github.com/hajimehoshi/ebiten/inpututil"
     
 )
 
 //setting game elements
 var player *ebiten.Image
 var floor *ebiten.Image
-
+var i float64
 //setting player position
 var(
     positionx float64 = 40.0
-    positiony float64 = 40.0
+    positiony float64 = 164.0
 )
 
 //opts is a global variable because it would not update correctly inside the "update" func
 var opts *ebiten.DrawImageOptions = &ebiten.DrawImageOptions{}
 
+func jump(){
+    for t:=time.Now(); time.Since(t)<(time.Second); {
+        if(positiony>140){
+            positiony -= 0.0001
+            opts.GeoM.Translate(0, -0.0001)
+        }
+    }
+    for t:=time.Now(); time.Since(t)<(time.Second); {
+        if(positiony<164){
+            positiony += 0.1
+            opts.GeoM.Translate(0, 0.1)
+        }
+    }
+    
+}
 
 // update is called every frame (1/60 [s]).
 func update(screen *ebiten.Image) error {
@@ -42,7 +59,7 @@ func update(screen *ebiten.Image) error {
     //creating player
     if player == nil {
         player, _ = ebiten.NewImage(16, 16, ebiten.FilterNearest)
-        opts.GeoM.Translate(40,40)
+        opts.GeoM.Translate(40,164)
     }
     
     player.Fill(color.White)
@@ -56,13 +73,8 @@ func update(screen *ebiten.Image) error {
         positionx -= 2
         opts.GeoM.Translate(-2, 0)
     }
-    if ebiten.IsKeyPressed(ebiten.KeyUp) && positiony > 0{
-        positiony -= 2
-        opts.GeoM.Translate(0, -2)
-    }
-    if ebiten.IsKeyPressed(ebiten.KeyDown) && positiony < 164{
-        positiony += 2
-        opts.GeoM.Translate(0, 2)
+    if inpututil.IsKeyJustPressed(ebiten.KeySpace) && positiony > 0{
+        go jump()
     }
     
     //defining floor image options
