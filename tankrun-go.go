7e7b64918ee37e5da175922fe31807824d477c44
gianluca <gianlucalup@gmail.com>
2019-05-28 17:30:52 +0200

    Added screen borders and floor 'solidity'

--- tankrun-go.go
+++ tankrun-go.go
@@ -4,20 +4,23 @@ import (
     "image/color"
     _ "image/png"
     "log"
+    "fmt"
     "github.com/hajimehoshi/ebiten"
     "github.com/hajimehoshi/ebiten/ebitenutil"
-    "github.com/hajimehoshi/ebiten/inpututil"
     
 )
 
+//setting game elements
 var player *ebiten.Image
 var floor *ebiten.Image
 
+//setting player position
 var(
-    positionx float64
-    positiony float64
+    positionx float64 = 40.0
+    positiony float64 = 40.0
 )
 
+//opts is a global variable because it would not update correctly inside the "update" func
 var opts *ebiten.DrawImageOptions = &ebiten.DrawImageOptions{}
 
 
@@ -26,32 +29,47 @@ func update(screen *ebiten.Image) error {
     
     screen.Fill(color.Black)
     
+    //only for debugging
+    fmt.Println(positionx, positiony)
+    
     var err error
+	//creating floor
 	floor, _, err = ebitenutil.NewImageFromFile("floor.png", ebiten.FilterDefault)
 	if err != nil {
 		log.Fatal(err)
 	}
     
+    //creating player
     if player == nil {
         player, _ = ebiten.NewImage(16, 16, ebiten.FilterNearest)
+        opts.GeoM.Translate(40,40)
     }
     
     player.Fill(color.White)
     
-    if ebiten.IsKeyPressed(ebiten.KeyRight){
-        opts.GeoM.Translate(positionx+(float64(inpututil.KeyPressDuration(ebiten.KeyRight))), 0)
+    //controls and screen borders
+    if ebiten.IsKeyPressed(ebiten.KeyRight) && positionx < 304{
+        positionx += 2
+        opts.GeoM.Translate(2, 0)
     }
-    if ebiten.IsKeyPressed(ebiten.KeyLeft){
-        opts.GeoM.Translate(positionx-(float64(inpututil.KeyPressDuration(ebiten.KeyLeft))), 0)
+    if ebiten.IsKeyPressed(ebiten.KeyLeft) && positionx > 0{
+        positionx -= 2
+        opts.GeoM.Translate(-2, 0)
     }
-    if ebiten.IsKeyPressed(ebiten.KeyUp){
-        opts.GeoM.Translate(0, positiony-(float64(inpututil.KeyPressDuration(ebiten.KeyUp))))
+    if ebiten.IsKeyPressed(ebiten.KeyUp) && positiony > 0{
+        positiony -= 2
+        opts.GeoM.Translate(0, -2)
     }
-    if ebiten.IsKeyPressed(ebiten.KeyDown){
-        opts.GeoM.Translate(0, positiony+(float64(inpututil.KeyPressDuration(ebiten.KeyDown))))
+    if ebiten.IsKeyPressed(ebiten.KeyDown) && positiony < 164{
+        positiony += 2
+        opts.GeoM.Translate(0, 2)
     }
+    
+    //defining floor image options
     optf := &ebiten.DrawImageOptions{}
+    
     optf.GeoM.Translate(0,180)
+    
     screen.DrawImage(floor, optf)
     screen.DrawImage(player, opts)
     
