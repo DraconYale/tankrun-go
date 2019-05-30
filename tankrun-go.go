f91204af1e8b27a17c186228ab0987b6f79b95d4
gianluca <gianlucalup@gmail.com>
2019-05-30 18:00:27 +0200

    Now jump is more real

--- tankrun-go.go
+++ tankrun-go.go
@@ -5,84 +5,118 @@ import (
     _ "image/png"
     "log"
     "fmt"
-    "time"
-    "sync"
+    //"math"
     "github.com/hajimehoshi/ebiten"
     "github.com/hajimehoshi/ebiten/ebitenutil"
     "github.com/hajimehoshi/ebiten/inpututil"
     
 )
 
+
+type Game struct {
+    //Player's position
+    positionx float64
+    positiony float64
+    vy float64 
+
+    // Camera
+    cameraX int
+    cameraY int
+
+}
+
+func NewGame() *Game {
+    g := &Game{}
+    g.in()
+    return g
+}
+
+func (g *Game) in() {
+    g.positionx = 40
+    g.positiony = 2600
+    g.cameraX = 0
+    g.cameraY = 0
+}
+
+//func (g *Game) init(){
+//    g.positionx = 40
+//    g.positiony = 164
+//    g.positionvy16 = 0
+//}
+
+func (g *Game) drawPlayer(screen *ebiten.Image) {
+	op := &ebiten.DrawImageOptions{}
+    h := 16
+	op.GeoM.Translate(g.positionx, -float64(h)/2.0)
+	op.GeoM.Translate(g.positionx, float64(h)/2.0)
+	op.GeoM.Translate(g.positionx, float64(g.positiony/16.0))
+    op.Filter = ebiten.FilterLinear
+	screen.DrawImage(player, op)
+}
+
 //setting game elements
 var player *ebiten.Image
 var floor *ebiten.Image
 var i float64
-//setting player position
-var(
-    positionx float64 = 40.0
-    positiony float64 = 164.0
-)
 
-//variables used to make the jump mutual exclusive
-var mux sync.Mutex
+
+//variable used to make the jump mutual exclusive
 var jump_count = 0
 
 //opts is a global variable because it would not update correctly inside the "update" func
 var opts *ebiten.DrawImageOptions = &ebiten.DrawImageOptions{}
 
-func jump(){
-    mux.Lock()
-    jump_count = 1
-    for t:=time.Now(); time.Since(t)<(time.Second); {
-        if(positiony>140){
-            positiony -= 0.0001
-            opts.GeoM.Translate(0, -0.0001)
-        }
-    }
-    for t:=time.Now(); time.Since(t)<(time.Second); {
-        if(positiony<164){
-            positiony += 0.0001
-            opts.GeoM.Translate(0, 0.0001)
-        }
-    }
-    jump_count = 0
-    mux.Unlock()
-}
+//func (g *Game) jump(){
+    
+//}
 
 // update is called every frame (1/60 [s]).
-func update(screen *ebiten.Image) error {
+func (g *Game) update(screen *ebiten.Image) error {
     
     screen.Fill(color.Black)
     
     //only for debugging
-    fmt.Println(positionx, positiony)
+    fmt.Println(g.positionx, g.positiony)
+    ebitenutil.DebugPrint(screen, fmt.Sprintf("TPS: %0.2f", ebiten.CurrentTPS()))
+    ebitenutil.DebugPrint(screen, fmt.Sprintf("\nFPS: %0.2f", ebiten.CurrentFPS()))
     
     var err error
-	//creating floor
-	floor, _, err = ebitenutil.NewImageFromFile("floor.png", ebiten.FilterDefault)
-	if err != nil {
-		log.Fatal(err)
-	}
+    //creating floor
+    floor, _, err = ebitenutil.NewImageFromFile("floor.png", ebiten.FilterDefault)
+    if err != nil {
+        log.Fatal(err)
+    }
     
     //creating player
     if player == nil {
         player, _ = ebiten.NewImage(16, 16, ebiten.FilterNearest)
-        opts.GeoM.Translate(40,164)
+        opts.GeoM.Translate(40,168)
     }
     
     player.Fill(color.White)
     
     //controls and screen borders
-    if ebiten.IsKeyPressed(ebiten.KeyRight) && positionx < 304{
-        positionx += 2
-        opts.GeoM.Translate(2, 0)
+    if ebiten.IsKeyPressed(ebiten.KeyRight) && g.positionx < 101{
+        g.positionx += 1
+        opts.GeoM.Translate(1, 0)
     }
-    if ebiten.IsKeyPressed(ebiten.KeyLeft) && positionx > 0{
-        positionx -= 2
-        opts.GeoM.Translate(-2, 0)
+    if ebiten.IsKeyPressed(ebiten.KeyLeft) && g.positionx > 0{
+        g.positionx -= 1
+        opts.GeoM.Translate(-1, 0)
     }
-    if inpututil.IsKeyJustPressed(ebiten.KeySpace) && positiony > 0 && jump_count == 0{
-        go jump()
+    if inpututil.IsKeyJustPressed(ebiten.KeySpace) && jump_count == 0 && g.positiony == 2630{
+        jump_count = 1
+        g.vy = -96
+        jump_count = 0
+    }
+    // Gravity
+    g.vy += 4
+    if g.vy > 96{
+        g.vy = 96
+    }
+    g.positiony += g.vy
+    if g.positiony > 2630{
+        g.positiony = 2630
     }
     
     //defining floor image options
@@ -91,14 +125,14 @@ func update(screen *ebiten.Image) error {
     optf.GeoM.Translate(0,180)
     
     screen.DrawImage(floor, optf)
-    screen.DrawImage(player, opts)
-    
+    g.drawPlayer(screen)
     return nil
 }
 
 func main() {
-    // Call ebiten.Run to start your game loop.
-    if err := ebiten.Run(update, 320, 240, 2, "TankRun"); err != nil {
-        panic(err)
-    }
+    g := NewGame()
+	
+	if err := ebiten.Run(g.update, 320, 240, 2, "TankRun"); err != nil {
+		panic(err)
+	}
 }
